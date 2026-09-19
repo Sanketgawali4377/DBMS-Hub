952e54232f483218698be40701784bff9dd14e19
sanket gawali <sanketgawali4377@gmail.com>
2026-09-19 17:00:40 +0530

    inserted some data into table

---
+++ SQL/01_basics/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO student VALUES(
+(1,"sanket gawali",15,"b1","chalisgoan"),
+(2,"jay jaiwal",19,"b2","sambhaji nagar"),
+(3,"kishori jagtap",10,"b2","nashik"),
+(4,"yogini firake",19,"b1","bhusaval"),
+(5,"tanushree gadekar",19,"b2","nashik"),
+(6,"payal kale",12,"b2","nashik"),
+(7,"pratiksha vadje",19,"b2","dindori"),
+(8,"dhanashree jadhav",16,"b2","nashik");
+);
