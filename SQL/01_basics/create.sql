a550f46a7403880813bf9bd8bda27a54dbc86ba4
sanket gawali <sanketgawali4377@gmail.com>
2026-09-19 16:40:48 +0530

    create table

---
+++ SQL/01_basics/create.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE college;
+use college;
+CREATE TABLE student(
+    roll_no INT primary,
+    name varchar(50),
+    age int,
+    batch varchar(10),
+    city varchar (15),
+     );
+
